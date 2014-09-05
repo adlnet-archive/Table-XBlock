7b1355f99a7b9f7c7ad335672402b8641ba9a7bc
Mick Muzac <mick.muzac.ctr@adlnet.gov>
2014-09-05 11:06:17 -0400

    Changing workstation, added initial studio view

---
+++ table/static/js/src/table_edit.js
@@ -0,0 +1,98 @@
+/* Javascript for TableXBlock. */
+function TableXBlock(runtime, element) {
+    var handlerUrl = runtime.handlerUrl(element, 'save_admin_settings');
+
+	$(element).find('.cancel-button').click(function() {
+        runtime.notify('cancel', {});
+    });
+	
+	$(element).find('.save-button').bind('click', function() {
+        runtime.notify('cancel', {});
+        
+        var handlerUrl = runtime.handlerUrl(element, 'update_dashboard');
+        var outObj = {};
+        
+        
+        outObj.width = $(element).find('#edit_width').val();
+        outObj.height = $(element).find('#edit_height').val();
+        outObj.display_name = $(element).find('#edit_display_name').val();
+        
+        $.post(handlerUrl, JSON.stringify(outObj), function(res){
+        
+			console.log("This is the response: ", res);
+		});
+        
+        
+    });
+	
+	var currentRow;
+	var bindObj = {
+		columns: ko.observableArray(),
+		rows: ko.observableArray(),
+		columnTypes: ["text", "textarea", "checkbox", "checkboxHighlight", "label"],
+		rowTypes: ["normal", "parent", "appendable", "parentAppendable"],
+		allowNewColumns: true,
+		allowNewRows: true,
+		tempRow: ko.observable(false),
+		addTempRow: function(obj){
+			bindObj.tempRow({type: ko.observable("normal"), value: ko.observable(""), isEditing: ko.observable(false)});
+			currentRow = obj;
+		},
+		editLabelClick: function(obj){
+			obj.isEditing(true);
+		},
+		editFieldKeypress: function(obj, e){			
+			//If the key pressed is Enter
+			if(e.keyCode == 13){
+				obj.isEditing(false);
+			}
+		},
+		pushRow: function(){
+			currentRow.children.push(bindObj.tempRow());
+			bindObj.cancelModal();
+		},
+		cancelModal: function(){
+			bindObj.tempRow(false);
+		},
+		addAppendableRow: function(obj){
+			var currentArr = bindObj.rows();
+			var i = bindObj.rows.indexOf(obj);
+			var number = obj.value.match(/[0-9]+/);
+			
+			if(i > -1 && Array.isArray(number)){
+				obj.type("parent");
+				bindObj.rows(currentArr.slice(0, i + 1));
+				bindObj.rows().push({type: ko.observable("parentAppendable"), value: obj.name + ' ' + (+number[0] + 1), name: obj.name, children: ko.observableArray()});
+				Array.prototype.push.apply(bindObj.rows(), currentArr.slice(i + 1, currentArr.length));
+				bindObj.rows.valueHasMutated();
+				
+				console.log(obj);
+			}
+			else console.log("Not found");
+		}
+	}
+	
+	bindObj.columns.subscribe(function(newVal){
+		var perc = bindObj.allowNewColumns ? 100 / (bindObj.columns().length + 1) : 100 / bindObj.columns().length;
+		$('.table_block .cell').not('#table_modal .cell').css('width', perc + '%');
+		
+		console.log(perc);
+	});
+	
+	bindObj.columns.push({name: "Exercise Name", placeholder: "[Enter exercise name]", type: "text"});
+	bindObj.columns.push({name: "Mobility Related?", type: "checkbox"});
+	bindObj.columns.push({name: "Random Label", type: "label"});
+	
+	bindObj.rows.push({type: ko.observable("parent"), value: "Ultimate Goal", children: ko.observableArray()});
+	bindObj.rows.push({type: ko.observable("parentAppendable"), value: "Week 1", name: 'Week', children: ko.observableArray()});
+	
+    //$(document).ready(function () {
+        /* Here's where you'd do things on page load. */
+		console.log("test");
+		$(element).on('click', '.editableLabel', function(){
+			$(this).hide();
+		});
+		//debugger;
+		ko.applyBindings(bindObj, (element instanceof $ ? element[0] : element));
+   // });
+}
--- table/table.py
+++ table/table.py
@@ -3,19 +3,19 @@
 import pkg_resources
 
 from xblock.core import XBlock
-from xblock.fields import Scope, Integer
+from xblock.fields import Scope, Integer, String
 from xblock.fragment import Fragment
 
 
 class TableXBlock(XBlock):
-    """
-    TO-DO: document what your XBlock does.
-    """
+	"""
+	TO-DO: document what your XBlock does.
+	"""
 
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
+	# Fields are defined on the class.  You can access them in your code as
+	# self.<fieldname>.
 	
-    _cell = '''<div class="cell">
+	_cell = '''<div class="cell">
 				<!-- ko if: $data.type == "label" -->
 					<span data-bind="text: name + ' col'"></span>
 				<!-- /ko -->
@@ -29,54 +29,66 @@ class TableXBlock(XBlock):
 					<input type="checkbox" />
 				<!-- /ko -->
 			</div>'''	
+			
+	tableStructure = String(default="", scope=Scope.settings, help="Options that will determine the table structure presented to users")
+	display_name = String(display_name="Display Name", default="Table XBlock", scope=Scope.settings, help="Name of the component in the edxplatform")
 
-    # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
-    )
+	# TO-DO: delete count, and define your own fields.
+	count = Integer(
+		default=0, scope=Scope.user_state,
+		help="A simple counter, to show something happening",
+	)
 
-    def resource_string(self, path):
-        """Handy helper for getting resources from our kit."""
-        data = pkg_resources.resource_string(__name__, path)
-        return data.decode("utf8")
+	def resource_string(self, path):
+		"""Handy helper for getting resources from our kit."""
+		data = pkg_resources.resource_string(__name__, path)
+		return data.decode("utf8")
 
-    # TO-DO: change this view to display your data your own way.
-    def student_view(self, context=None):
-        """
-        The primary view of the TableXBlock, shown to students
-        when viewing courses.
-        """
-        html = self.resource_string("static/html/table.html")
-        frag = Fragment(html.format(self=self))
-        frag.add_css(self.resource_string("static/css/table.css"))
-        frag.add_javascript(self.resource_string("static/js/lib/knockout-3.1.0.js"))
-        frag.add_javascript(self.resource_string("static/js/src/table.js"))
-        frag.initialize_js('TableXBlock')
-        return frag
+	# TO-DO: change this view to display your data your own way.
+	def student_view(self, context=None):
+		"""
+		The primary view of the TableXBlock, shown to students
+		when viewing courses.
+		"""
+		html = self.resource_string("static/html/table.html")
+		frag = Fragment(html.format(self=self))
+		frag.add_css(self.resource_string("static/css/table.css"))
+		frag.add_javascript(self.resource_string("static/js/lib/knockout-3.1.0.js"))
+		frag.add_javascript(self.resource_string("static/js/src/table.js"))
+		frag.initialize_js('TableXBlock')
+		return frag
+		
+	# TO-DO: change this view to display your data your own way.
+	def studio_view(self, context=None):
+		html = self.resource_string("static/html/table_edit.html")
+		frag = Fragment(html.format(self=self))
+		frag.add_javascript(self.resource_string("static/js/lib/knockout-3.1.0.js"))
+		
+		frag.initialize_js('TableXBlock')
+		return frag
 
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
-    @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
-        """
-        An example handler, which increments the data.
-        """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
+	# TO-DO: change this handler to perform your own actions.  You may need more
+	# than one handler, or you may not need any handlers at all.
+	@XBlock.json_handler
+	def save_admin_settings(self, data, suffix=''):
+		"""
+		An example handler, which increments the data.
+		"""
+		# Just to show data coming in...
+		#assert data['hello'] == 'world'
 
-        self.count += 1
-        return {"count": self.count}
+		self.count += 1
+		return {"count": self.count}
 
-    # TO-DO: change this to create the scenarios you'd like to see in the
-    # workbench while developing your XBlock.
-    @staticmethod
-    def workbench_scenarios():
-        """A canned scenario for display in the workbench."""
-        return [
-            ("TableXBlock",
-             """<vertical_demo>
-                <table/>
-                </vertical_demo>
-             """),
-        ]
+	# TO-DO: change this to create the scenarios you'd like to see in the
+	# workbench while developing your XBlock.
+	@staticmethod
+	def workbench_scenarios():
+		"""A canned scenario for display in the workbench."""
+		return [
+			("TableXBlock",
+			 """<vertical_demo>
+				<table/>
+				</vertical_demo>
+			 """),
+		]
